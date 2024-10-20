cab65dc1a1997b6664b4b3e531ea29b25a66ecdb
Nitin Sri Sai C <nitinsrisai05@gmail.com>
2024-10-20 14:08:12 +0000

    change1

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,6 +8,8 @@ int fibonacci(int n) {
 }
 
 int main(int argc, char* argv[]) {
+
+    std::cout << "Hii!";
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
         return 1; // Exit with error code
